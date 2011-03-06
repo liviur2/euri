4acb388ec0ad7c28af0f3768f85f13d3a5a4c87d
Liviu Radoi <keller946@yahoo.com>
2011-03-06 20:22:44 +0200

    Starting app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Euri::Application.config.secret_token = '1b112b13e090e791dda395fdf2cfc7276267552550e5943b9f4887daff3013ac332a47e228c6864b20eb2d3fb9ba1066fdc1a8b5cf726a57326a4e8c57bbf7cc'
